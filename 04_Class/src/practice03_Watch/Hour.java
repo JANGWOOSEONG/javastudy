cf4feb110a82524a2958368def79e1974af1e630
JANGWOOSEONG <wkddntjd1222@naver.com>
2023-01-13 17:24:28 +0900

    2023.01.13

---
+++ 04_Class/src/practice03_Watch/Hour.java
@@ -0,0 +1,5 @@
+package practice03_Watch;
+
+public class Hour extends Watch {
+
+}
---
+++ 04_Class/src/practice03_Watch/Watch.java
@@ -0,0 +1,52 @@
+package practice03_Watch;
+
+public class Watch {
+	
+	Watch watch;
+	
+    // 필드
+	private int hour;  // 0 ~ 23
+	private int minute;  // 0 ~ 59
+	private int second;  // 0 ~ 59
+	
+	// 생성자
+	public Watch(int hour, int minute, int second) {
+		this.hour = hour;  
+		this.minute = minute;
+		this.second = second;
+	}
+	// 메소드
+
+	public Watch getWatch() {
+		return watch;
+	}
+
+	public void setWatch(Watch watch) {
+		this.watch = watch;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public int getMinute() {
+		return minute;
+	}
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+	public int getSecond() {
+		return second;
+	}
+
+	public void setSecond(int second) {
+		this.second = second;
+	}
+
+}
